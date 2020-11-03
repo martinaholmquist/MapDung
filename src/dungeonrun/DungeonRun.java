3705b92cd590d95e29558e8ecb3052a980b5a20c
marti <marti@DESKTOP-FMACCDF>
2020-11-03 16:14:45 +0100

    skickar vår första fil av map

---
+++ src/dungeonrun/DungeonRun.java
@@ -0,0 +1,47 @@
+package dungeonrun;
+
+import java.util.*;
+
+public class DungeonRun {
+
+    public static void main(String[] args) {
+        map w = new map();  //skapar objekt av klassen map
+        w.welcomeMessage();
+        w.showMap();
+       //w.pictureMap();
+       //w.pictureMap2();
+     //  w.showMapSize();
+       w.mapSmall();
+       w.choiceOfDirection();
+       //w.map2();
+       
+       w.visitedRoom();
+       
+      
+       
+       /*
+       
+       //while (true)
+       System.out.println("\nhär händer det ev grejer, monster å skatter å sånt.... ");
+       
+       
+       for (int k = 0; k < mapSmall.length; k++) {
+       for (int l = 0; l < mapSmall.length; l++) {
+       if (mapSmall[k][l] == direction){
+       mapSmall[k][l] = 0;
+       System.out.println("fortsätt spelet" + mapSmall[k][l]);
+       }
+       }
+       }
+       */
+       /*System.out.println("Dags att gå vidare....");
+       if (direction == mapSmall[0][0] ||  direction == mapSmall[0][1] || direction == mapSmall[0][2] || direction == mapSmall[0][3] );
+       {
+       System.out.println("Då kan du välja h, n");
+       } else if
+       //då kan du gå vänster eller neråt
+       
+       }*/
+       
+}
+}
---
+++ src/dungeonrun/HittePa.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dungeonrun;
+
+/**
+ *
+ * @author marti
+ */
+public class HittePa {
+    /*  int[][] mapSmall = new int[4][4];
+
+        mapSmall[0][0]= 1;
+        mapSmall[0][1]= 2;
+        mapSmall[0][2]= 3;
+        mapSmall[0][3]= 4;
+    
+        mapSmall[1][0]= 5;
+        mapSmall[1][1]= 6;
+        mapSmall[1][2]= 7;
+        mapSmall[1][3]= 8;
+    
+        mapSmall[2][0]= 9;
+        mapSmall[2][1]= 10;
+        mapSmall[2][2]= 11;
+        mapSmall[2][3]= 12;
+    
+        mapSmall[3][0]= 13;
+        mapSmall[3][1]= 14;
+        mapSmall[3][2]= 15;
+        mapSmall[3][3]= 16;
+    
+        int rows = 4;
+        int columns = 4;
+        int i, j;
+    
+        for ( i = 0; i < rows; i++) {
+        for ( j = 0; j < columns; j++) {
+        System.out.println(mapSmall[i][j] + " ");
+        }
+        System.out.println( "" );
+        }*/
+    
+}
